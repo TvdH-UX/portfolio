723b658121abda7f476ea64b01808dd5152a95dd
Thomas van den Heuvel <t.vandenheuvel@qquest.nl>
2023-08-17 11:47:58 +0200

    Final touches home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,13 @@ export function Home() {
       <div className={styles.mainSection}>
         <h1 className={styles.greeting}>Hoi, ik ben</h1>
         <h1 className={styles.name}>Thomas van den Heuvel</h1>
-        <h2>Developer | Tester | UX/UI Designer</h2>
+        <div className={styles.roles}>
+          <h2>Developer</h2>
+          <h2>|</h2>
+          <h2>Tester</h2>
+          <h2>|</h2>
+          <h2>UX/UI Designer</h2>
+        </div>
         <div className={styles.clickThroughLinks}>
           <ul>
             <li>
