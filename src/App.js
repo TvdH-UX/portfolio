44db38f5f75dc3297803beb8a93cb83dc4925116
Thomas van den Heuvel <t.vandenheuvel@qquest.nl>
2023-08-23 13:29:02 +0200

    follower dot

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styles from "./App.module.css";
+import { useRef } from "react";
 import { Route, Routes, HashRouter } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { MijnWerk } from "./pages/MijnWerk";
@@ -10,10 +11,16 @@ import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 
 function App() {
+  const cursor = useRef(null);
+  const changePosition = (e) => {
+    cursor.current.style.top = `${e.clientY}px`;
+    cursor.current.style.left = `${e.clientX}px`;
+  };
   return (
     <div>
+      <div className={`${styles.cursor} ${styles.dot}`} ref={cursor}></div>
       <div className={styles.grain}></div>
-      <div className={styles.container}>
+      <div className={styles.container} onMouseMove={changePosition}>
         <div className={styles.body}>
           <NavBar />
           <Routes>
